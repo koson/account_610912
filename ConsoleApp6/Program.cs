036245cd047e4caebcf7bb785611d1375c7f6591
Koson Trachu <koson.trachu@gmail.com>
2018-09-12 11:50:55 +0700

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account ac = new Account("qwert1234", 10000);
+            ac.Display();
+            ac.Debit(300);
+            ac.Display();
+            ac.Credit(5000);
+            ac.Debit(20000);
+            ac.Display();
+        }
+    }
+
+    public class Account
+    {
+        private string accountName;
+        private int accountBalance;
+        private int creditBalance;
+
+        public void Credit(int amount)
+        {
+            Console.WriteLine("Credit {0} bath", amount);
+            creditBalance += amount;
+        }
+        public void Debit(int amount)
+        {
+            if (amount > accountBalance)
+                Console.WriteLine("Sorry, there is not enough money");
+            else
+                accountBalance -= amount;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Name: {0}, Balance: {1 , 20:C}", 
+                accountName, accountBalance);
+        }
+
+        public Account(string accountName, int amount)
+        {
+            this.accountName = accountName;
+            accountBalance = amount;
+        }
+
+
+    }
+
+}
